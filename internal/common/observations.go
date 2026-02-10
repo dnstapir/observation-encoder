b5dda84b4ce46c6631eaeb139ec055b639476f31
Leon Fernandez <leon.fernandez@internetstiftelsen.se>
2026-02-10 15:26:44 +0100

    avoid duplicating observation encodings

--- internal/common/observations.go
+++ internal/common/observations.go
@@ -4,6 +4,6 @@ const OBS_GLOBALLY_NEW uint32 = 1
 const OBS_LOOPTEST uint32 = 1024
 
 var OBS_MAP = map[string]uint32{
-	"globally_new": 1,
-	"looptest":     1024,
+	"globally_new": OBS_GLOBALLY_NEW,
+	"looptest":     OBS_LOOPTEST,
 }
